01774320af545b7744e8b3c244ea7a690ca1239b
Jesse McReady <jessemcready@gmail.com>
2018-04-17 16:15:43 -0400

    db url fix

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://jesse:jesse>@ds247569.mlab.com:47569/meanauthpractice',
+  database: 'mongodb://jesse:jesse@ds247569.mlab.com:47569/meanauthpractice',
   secret: 'yoursecret'
 }
